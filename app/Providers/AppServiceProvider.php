6f469502f99a11fd9f4551b7a255e0b4769ac1cc
Godsend Joseph <bobbyjoseph108@gmail.com>
2025-02-10 14:29:53 +0000

    set up configuration for Application Service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +24,52 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureUrl();
+    }
+
+    /**
+     * Configure application commands.
+     */
+    private function configureCommands(): void
+    {
+        /** @var \Illuminate\Foundation\Application $app */
+        $app = $this->app;
+        DB::prohibitDestructiveCommands(
+            $app->isProduction()
+        );
+    }
+
+    /**
+     * Configure application models.
+     */
+    private function configureModels(): void
+    {
+        Schema::defaultStringLength(191);
+
+        Model::shouldBeStrict();
+        Model::unguard();
+
+        // in production, log lazy loading violation instead.
+
+        /** @var \Illuminate\Foundation\Application $app */
+        $app = $this->app;
+
+        if ($app->isProduction()) {
+            Model::handleLazyLoadingViolationUsing(function ($model, $relation) {
+                $class = get_class($model);
+
+                info("Attempted to lazy load [{$relation} on model [{$class}]]");
+            });
+        }
+    }
+
+    /**
+     * Configure application models.
+     */
+    private function configureUrl(): void
+    {
+        URL::forceScheme("https");
     }
 }
